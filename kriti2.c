309e9a0ebda71db6f9c0e307ba353ad09c936893
KIRUTHIKA555 <kiruthikakt38@gmail.com>
2017-06-29 14:09:05 +0530

    Create kriti2.c

---
+++ kriti2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n enter the numbers");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n the given number is even");
+}
+else
+{
+printf("\n the given number is odd");
+}
+getch();
+}
